0e3e46da42e8647b9f30137a9ad9e07108b69c41
ian <iankush1317@gmail.com>
2022-08-20 22:42:22 -0400

    contact form, add image from astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify/functions";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), image()],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img", "svgo"]
