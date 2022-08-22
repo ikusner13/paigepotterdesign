bff0fcfb0d82b954e750099d69b0fbd401c20cfa
ian <iankush1317@gmail.com>
2022-08-21 22:03:46 -0400

    test image again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import image from "@astrojs/image";
 // import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ubiquitous-pony-627bfd.netlify.app/",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), image()],
   // output: "server",
   // adapter: netlify(),
   vite: {
