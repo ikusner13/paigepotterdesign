125ab7b602e20d1ff26a85d157255bd630cff533
ian <iankush1317@gmail.com>
2022-08-21 20:38:36 -0400

    seeing if config changes fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify/functions";
-
-import image from "@astrojs/image";
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), react(), image()],
-  output: "server",
-  adapter: netlify(),
+  site: "https://ubiquitous-pony-627bfd.netlify.app/",
+  integrations: [tailwind(), react()],
+  // output: "server",
+  // adapter: netlify(),
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img"],
